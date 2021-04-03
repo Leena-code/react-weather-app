1e9b07c29e5e4074a0e142c1a4221336f41765aa
Leena-code <76698949+Leena-code@users.noreply.github.com>
2021-04-03 19:38:06 +0200

    Changed css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -11,13 +11,13 @@ export default function WeatherInfo(props) {
     return (
         <div className="weatherInfo">
             <div className="row weather">
-                <div className="col-6 city pl-0 pr-0">
+                <div className="col-5 city pl-0 pr-0">
            <h1>{props.data.city}, {props.data.country}</h1>
               <h2>
                   <FormattedDate date= {props.data.date} />
               </h2>
               </div>
-          <div className="col-6 weatherData pl-0 pr-0">
+          <div className="col-7 weatherData pl-0 pr-0">
               <div className="clearfix">
                  <span className="float-left">
                    <span className= "currentWeatherIcon">
@@ -42,7 +42,7 @@ export default function WeatherInfo(props) {
                   </div>
                   </div>
                   </div>
-                <div className="col-5 weatherDescriptionDetail">
+                <div className="col-5 weatherDescriptionDetail sunrise pl-0 pr-0">
                   <ul>
               <li>
                 Humidity: {props.data.humidity} %
